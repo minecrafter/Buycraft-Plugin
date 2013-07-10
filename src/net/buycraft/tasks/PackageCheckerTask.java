73dd240d037e45b90be5e416e865b47a3ebc1583
ShadowDog007 <shadowdog007@forgenz.com>
2013-07-10 17:45:59 +1200

    Make the package checker run asynchronously
    Because why would you do it any other way?

--- src/net/buycraft/tasks/PackageCheckerTask.java
+++ src/net/buycraft/tasks/PackageCheckerTask.java
@@ -3,6 +3,7 @@ package net.buycraft.tasks;
 import net.buycraft.Plugin;
 import net.buycraft.api.ApiTask;
 import net.buycraft.util.Chat;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -10,6 +11,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PackageCheckerTask extends ApiTask {
     private Plugin plugin;
@@ -29,69 +31,71 @@ public class PackageCheckerTask extends ApiTask {
         try {
             if (plugin.isAuthenticated(null)) {
                 if (plugin.getSettings().getBoolean("commandChecker") || manualExecution) {
-                    if (plugin.getServer().getOnlinePlayers().length > 0 || manualExecution) {
-                        final JSONObject apiResponse = plugin.getApi().commandsGetAction();
-                        Runnable r = new Runnable () {
-                            public void run() {
-                                try {
-                                    if (apiResponse != null && apiResponse.getInt("code") == 0) {
-                                        JSONObject apiPayload = apiResponse.getJSONObject("payload");
-                                        JSONArray commandsPayload = apiPayload.getJSONArray("commands");
+                	// Fetch online player list
+                	Player[] onlinePlayers = plugin.getServer().getOnlinePlayers();
+                    if (onlinePlayers.length > 0 || manualExecution) {
+                    	// Put players in a HashMap for quick access
+                    	HashMap<String, Player> onlinePlayerSet = new HashMap<String, Player>(onlinePlayers.length);
+                    	for (Player player : onlinePlayers) {
+                    		onlinePlayerSet.put(player.getName(), player);
+                    	}
+                    	
+                        JSONObject apiResponse = plugin.getApi().commandsGetAction();
+                        
+                        try {
+                        	if (apiResponse != null && apiResponse.getInt("code") == 0) {
+                        		JSONObject apiPayload = apiResponse.getJSONObject("payload");
+                        		JSONArray commandsPayload = apiPayload.getJSONArray("commands");
 
-                                        ArrayList<String> executedCommands = new ArrayList<String>();
+                        		ArrayList<String> executedCommands = new ArrayList<String>();
 
-                                        for (int i = 0; i < commandsPayload.length(); i++) {
-                                        	JSONObject row = commandsPayload.getJSONObject(i);
+                        		for (int i = 0; i < commandsPayload.length(); i++) {
+                        			JSONObject row = commandsPayload.getJSONObject(i);
 
-                                        	String username = row.getString("ign");
-                                        	Boolean requireOnline = row.getBoolean("requireOnline");
-                                        	String command = row.getJSONArray("commands").getString(0);
+                        			String username = row.getString("ign");
+                        			Boolean requireOnline = row.getBoolean("requireOnline");
+                        			String command = row.getJSONArray("commands").getString(0);
 
-                                        	Player currentPlayer = plugin.getServer().getPlayer(username);
+                        			if (requireOnline == false || onlinePlayerSet.containsKey(username)) {
+                        				if (executedCommands.contains(username) == false) {
+                        					executedCommands.add(username);
+                        				}
+                        				final String c = command;
+                        				final String u = username;
+                        				Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                        					public void run() {
+                        						CommandExecuteTask.call(c, u);
+                        					}
+                        				}, 60L);
+                        			}
+                        		}
 
-                                        	if (currentPlayer != null || requireOnline == false) {
-                                        		if (executedCommands.contains(username) == false) {
-                                        			executedCommands.add(username);
-                                        		}
-                                        		final String c = command;
-                                        		final String u = username;
-                                        		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-                                        			public void run() {
-                                        				CommandExecuteTask.call(c, u);
-                                        			}
-                                        		}, 60L);
-                                        	}
-                                        }
+                        		if (executedCommands.size() > 0) {
+                        			for (String username : executedCommands) {
+                        				Player player = onlinePlayerSet.get(username);
 
-                                        if (executedCommands.size() > 0) {
-                                            for (String username : executedCommands) {
-                                                Player player = plugin.getServer().getPlayer(username);
+                        				if (player != null) {
 
-                                                if (player != null) {
-                                                	
-                                                	player.sendMessage(new String[] {
-                                                		Chat.header(), 
-                                                		Chat.seperator(),
-                                                        Chat.seperator() + ChatColor.GREEN + plugin.getLanguage().getString("commandsExecuted"),
-                                                        Chat.seperator(), 
-                                                        Chat.footer()
-                                                    });
-                                                }
-                                            }
+                        					player.sendMessage(new String[] {
+                        							Chat.header(), 
+                        							Chat.seperator(),
+                        							Chat.seperator() + ChatColor.GREEN + plugin.getLanguage().getString("commandsExecuted"),
+                        							Chat.seperator(), 
+                        							Chat.footer()
+                        					});
+                        				}
+                        			}
 
-                                            CommandDeleteTask.call(new JSONArray(executedCommands.toArray()).toString());
-                                        }
+                        			CommandDeleteTask.call(new JSONArray(executedCommands.toArray()).toString());
+                        		}
 
-                                        plugin.getLogger().info("Package checker successfully executed.");
-                                    } else {
-                                        plugin.getLogger().severe("No response/invalid key during package check.");
-                                    }
-                                } catch(Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        };
-                        sync(r);
+                        		plugin.getLogger().info("Package checker successfully executed.");
+                        	} else {
+                        		plugin.getLogger().severe("No response/invalid key during package check.");
+                        	}
+                        } catch(Exception e) {
+                        	e.printStackTrace();
+                        }
                     }
                 }
             }
