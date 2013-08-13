19abb70cfb90e46d71dc47991f7729d9c848dffe
ShadowDog007 <shadowdog007@forgenz.com>
2013-08-14 01:27:16 +1200

    Fixed random tabs
    They were annoying me. So I killed them. >:D

--- src/net/buycraft/tasks/PackageCheckerTask.java
+++ src/net/buycraft/tasks/PackageCheckerTask.java
@@ -36,65 +36,65 @@ public class PackageCheckerTask extends ApiTask {
             lastExecution = System.currentTimeMillis();
             if (plugin.isAuthenticated(null)) {
                 if (plugin.getSettings().getBoolean("commandChecker") || manualExecution) {
-                	// Fetch online player list
-                	Player[] onlinePlayers = plugin.getServer().getOnlinePlayers();
+                    // Fetch online player list
+                    Player[] onlinePlayers = plugin.getServer().getOnlinePlayers();
                     if (onlinePlayers.length > 0 || manualExecution) {
-                    	
+                        
                         JSONObject apiResponse = plugin.getApi().commandsGetAction();
                         
-                        	if (apiResponse != null && apiResponse.getInt("code") == 0) {
-                        		JSONObject apiPayload = apiResponse.getJSONObject("payload");
-                        		JSONArray commandsPayload = apiPayload.getJSONArray("commands");
+                            if (apiResponse != null && apiResponse.getInt("code") == 0) {
+                                JSONObject apiPayload = apiResponse.getJSONObject("payload");
+                                JSONArray commandsPayload = apiPayload.getJSONArray("commands");
 
-                        		LinkedHashSet<String> executedCommands = new LinkedHashSet<String>();
+                                LinkedHashSet<String> executedCommands = new LinkedHashSet<String>();
 
-                        		for (int i = 0; i < commandsPayload.length(); i++) {
-                        			JSONObject row = commandsPayload.getJSONObject(i);
+                                for (int i = 0; i < commandsPayload.length(); i++) {
+                                    JSONObject row = commandsPayload.getJSONObject(i);
 
-                        			String username = row.getString("ign");
-                        			Boolean requireOnline = row.getBoolean("requireOnline");
-                        			String command = row.getJSONArray("commands").getString(0);
+                                    String username = row.getString("ign");
+                                    boolean requireOnline = row.getBoolean("requireOnline");
+                                    String command = row.getJSONArray("commands").getString(0);
                                     int delay = row.getInt("delay");
 
-                        			if (requireOnline == false || getPlayer(onlinePlayers, username) != null) {
-                        				executedCommands.add(username);
-                        					
-                        				String c = command;
-                        				String u = username;
-                        				
-                        				Plugin.getInstance().getCommandExecutor().queueCommand(c, u, delay);
-                        			}
-                        		}
-                        		
-                        		// If the plugin is disabled here our commands won't get executed so we return
-                        		if (!Plugin.getInstance().isEnabled()) {
-                        		    return;
-                        		}
-                        		
-                        		Plugin.getInstance().getCommandExecutor().scheduleExecutor();
+                                    if (requireOnline == false || getPlayer(onlinePlayers, username) != null) {
+                                        executedCommands.add(username);
+                                            
+                                        String c = command;
+                                        String u = username;
+                                        
+                                        Plugin.getInstance().getCommandExecutor().queueCommand(c, u, delay);
+                                    }
+                                }
+                                
+                                // If the plugin is disabled here our commands won't get executed so we return
+                                if (!Plugin.getInstance().isEnabled()) {
+                                    return;
+                                }
+                                
+                                Plugin.getInstance().getCommandExecutor().scheduleExecutor();
 
-                        		if (executedCommands.size() > 0) {
-                        			for (String username : executedCommands) {
-                        				Player player = getPlayer(onlinePlayers, username);
+                                if (executedCommands.size() > 0) {
+                                    for (String username : executedCommands) {
+                                        Player player = getPlayer(onlinePlayers, username);
 
-                        				if (player != null) {
-                        					player.sendMessage(new String[] {
-                        							Chat.header(), 
-                        							Chat.seperator(),
-                        							Chat.seperator() + ChatColor.GREEN + plugin.getLanguage().getString("commandsExecuted"),
-                        							Chat.seperator(), 
-                        							Chat.footer()
-                        					});
-                        				}
-                        			}
+                                        if (player != null) {
+                                            player.sendMessage(new String[] {
+                                                    Chat.header(), 
+                                                    Chat.seperator(),
+                                                    Chat.seperator() + ChatColor.GREEN + plugin.getLanguage().getString("commandsExecuted"),
+                                                    Chat.seperator(), 
+                                                    Chat.footer()
+                                            });
+                                        }
+                                    }
 
-                        			CommandDeleteTask.call(new JSONArray(executedCommands.toArray()).toString());
-                        		}
+                                    CommandDeleteTask.call(new JSONArray(executedCommands.toArray()).toString());
+                                }
 
-                        		plugin.getLogger().info("Package checker successfully executed.");
-                        	} else {
-                        		plugin.getLogger().severe("No response/invalid key during package check.");
-                        	}
+                                plugin.getLogger().info("Package checker successfully executed.");
+                            } else {
+                                plugin.getLogger().severe("No response/invalid key during package check.");
+                            }
                     }
                 }
             }
